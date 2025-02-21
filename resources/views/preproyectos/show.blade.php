f067f5f5ea8f37b3c6b68b720768525e16e20b3c
Josue Cardona lp <ingjosue.cardona@gmail.com>
2025-02-21 12:49:28 -0600

    test

--- resources/views/preproyectos/show.blade.php
+++ resources/views/preproyectos/show.blade.php
@@ -11,7 +11,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                        @livewire('preproyectos.manage-preprojects', ['preProyectoId' => $preproyecto->id])
+                        @livewire('preproyectos.edit-pre-project', ['preProyectoId' => $preproyecto->id])
                 </div>
             </div>
         </div>
