f050c6889bb87b20088cc80ffb942be7c9facd08
Josue Cardona lp <ingjosue.cardona@gmail.com>
2025-02-24 16:57:54 -0600

    test

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -108,8 +108,8 @@
             
                                                 <div class="grid grid-cols-1 md:grid-cols-2">
                                                     <div>
-                                                        <livewire:pedidos.orden-produccion-pdf :proyectoId="$proyecto->id" />
-    
+                                                       
+                                                            @livewire('pedidos.orden-produccion-pdf', ['proyectoId' => $proyecto->id])
                                                     </div>
                                                     <div>
                                                         <div class="mt-4">
