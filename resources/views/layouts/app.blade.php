37284a5a60fd65c46870159813a52f4edf9cfa68
Josue Cardona lp <ingjosue.cardona@gmail.com>
2025-02-24 11:38:47 -0600

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <livewire:styles />
-
+        <base href="{{ config('app.url') }}/" />
         <script>
             document.addEventListener('livewire:init', () => {
                 const baseUrl = '{{ url('/') }}';
