89269d6d18fbefa4faeb166bea8c621c667d17af
Josue Cardona lp <ingjosue.cardona@gmail.com>
2025-03-28 16:20:17 -0600

    change demo

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -8,19 +8,23 @@
 <body>
     <h1>Escuchando "canal-demo"...</h1>
 
+    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
     <script>
         const pusher = new Pusher('clave-demo-websockets', {
             cluster: 'mt1',
             wsHost: window.location.hostname,
             wsPort: 6001,
             forceTLS: false,
+            encrypted: false,
+            enabledTransports: ['ws'], // 🔥 IMPORTANTE: solo WS (no WSS)
             disableStats: true
         });
-
+    
         const channel = pusher.subscribe('canal-demo');
-        channel.bind('evento.demo', function(data) {
+        channel.bind('evento.demo', function (data) {
             console.log('📡 Evento recibido:', data.mensaje);
-        }); 
+        });
     </script>
+    
 </body>
 </html>
