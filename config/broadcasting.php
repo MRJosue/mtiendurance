993a8bb3a6ee71eda6c9a6559d3d709eb6dfb957
Josue Cardona lp <ingjosue.cardona@gmail.com>
2025-03-28 15:33:03 -0600

    test n

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,11 +37,20 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+                // 'cluster' => env('PUSHER_APP_CLUSTER'),
+                // 'useTLS' => false,
+                // 'host' => '127.0.0.1',
+                // 'port' => 6001,
+                // 'scheme' => 'http',
+
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
                 'useTLS' => false,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'https',
+                'encrypted' => false,
+
+
             ],
         ],
 
