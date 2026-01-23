1d03e16600204d931dd4721738d2c2aa83e0a747
Josue Cardona <ingjosue.cardona@gmail.com>
2026-01-23 11:23:36 -0600

    cambio js

---
+++ resources/js/app-tst.js
@@ -0,0 +1,10 @@
+import '../css/app.css';
+import './bootstrap';
+
+import Dropzone from 'dropzone';
+import 'dropzone/dist/dropzone.css';
+
+import imageZoom from './components/image-zoom';
+window.imageZoom = imageZoom;
+
+console.log('Hola soy app js');
