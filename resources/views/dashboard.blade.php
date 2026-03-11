a5fc3701ff9baeb6091578ed2f86cab5747e782a
Josue Cardona <ingjosue.cardona@gmail.com>
2026-03-11 11:36:46 -0600

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
         </div>
         @endcan
 
-        @canany(['dashboardclienteproyectos', 'dashboardjefediseñadorproyectos'])
+        @canany('dashboardclienteproyectos')
         <div class="w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">       
