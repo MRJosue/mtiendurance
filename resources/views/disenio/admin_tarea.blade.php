d2d9be3142ff559c35971717f5467f971ee65a3f
Josue Cardona <ingjosue.cardona@gmail.com>
2026-03-19 13:54:59 -0600

    tareas

--- resources/views/disenio/admin_tarea.blade.php
+++ resources/views/disenio/admin_tarea.blade.php
@@ -11,7 +11,9 @@
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                                <livewire:disenio.administrar-tareas />
+                              @livewire('tareas.administra-tareas')
+                              
+                                {{-- <livewire:disenio.administrar-tareas /> --}}
 
                         </div>
                      
