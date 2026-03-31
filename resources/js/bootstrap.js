8e5902eee1c8a1897bc43e22cc875bd8dc8f1e01
Josue Cardona <ingjosue.cardona@gmail.com>
2026-03-31 17:39:52 -0600

    add broad c

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,25 +1,18 @@
 import axios from 'axios';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//   broadcaster: 'reverb',
-//   key: import.meta.env.VITE_REVERB_APP_KEY,
-//   wsHost: import.meta.env.VITE_REVERB_HOST,
-//   wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-//   wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-//   forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
-//   enabledTransports: ['ws', 'wss'],
-// });
-
-// console.log('Hola soy Bootrap js', {
-//   key: import.meta.env.VITE_REVERB_APP_KEY,
-//   host: import.meta.env.VITE_REVERB_HOST,
-//   port: import.meta.env.VITE_REVERB_PORT,
-//   scheme: import.meta.env.VITE_REVERB_SCHEME,
-// });
+window.Echo = new Echo({
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
+  wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 443),
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
+});
